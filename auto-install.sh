757fa672e79a36e8ed648862b3b657d9696c809d
Alvaro Lopez Medina <alopezme@redhat.com>
2021-05-12 20:56:27 +0200

    Improve code indentation

--- auto-install.sh
+++ auto-install.sh
@@ -50,18 +50,24 @@ fi
 
 # Deploy the RHDG operator
 echo -e "\n[1/8]Deploying the RHDG operator"
-oc process -f templates/rhdg-01-operator.yaml -p OPERATOR_NAMESPACE=$RHDG_OPERATOR_NAMESPACE -p CLUSTER_NAMESPACE=$RHDG_NAMESPACE | oc apply -f -
+oc process -f templates/rhdg-01-operator.yaml \
+    -p OPERATOR_NAMESPACE=$RHDG_OPERATOR_NAMESPACE \
+    -p CLUSTER_NAMESPACE=$RHDG_NAMESPACE | oc apply -f -
 
 echo -n "Waiting for pods ready..."
 while [[ $(oc get pods -l name=infinispan-operator-alm-owned -n $RHDG_OPERATOR_NAMESPACE -o 'jsonpath={..status.conditions[?(@.type=="Ready")].status}') != "True" ]]; do echo -n "." && sleep 1; done; echo -n -e "  [OK]\n"
 
 # Deploy the RHDG cluster
 echo -e "\n[2/8]Deploying the RHDG cluster"
-oc process -f templates/rhdg-02-cluster.yaml -p CLUSTER_NAMESPACE=$RHDG_NAMESPACE -p CLUSTER_NAME=$RHDG_CLUSTER_NAME | oc apply -f -
+oc process -f templates/rhdg-02-cluster.yaml \
+    -p CLUSTER_NAMESPACE=$RHDG_NAMESPACE \
+    -p CLUSTER_NAME=$RHDG_CLUSTER_NAME | oc apply -f -
 
 # Create basic caches
 echo -e "\n[3/8]Creating basic caches"
-oc process -f templates/rhdg-03-caches.yaml -p CLUSTER_NAMESPACE=$RHDG_NAMESPACE -p CLUSTER_NAME=$RHDG_CLUSTER_NAME | oc apply -f -
+oc process -f templates/rhdg-03-caches.yaml \
+    -p CLUSTER_NAMESPACE=$RHDG_NAMESPACE \
+    -p CLUSTER_NAME=$RHDG_CLUSTER_NAME | oc apply -f -
 
 ##
 # 2) PROMETHEUS
@@ -69,7 +75,9 @@ oc process -f templates/rhdg-03-caches.yaml -p CLUSTER_NAMESPACE=$RHDG_NAMESPACE
 
 # Configure Prometheus to monitor RHDG
 echo -e "\n[4/8]Configure Prometheus to monitor RHDG"
-oc process -f templates/rhdg-04-monitoring.yaml -p CLUSTER_NAMESPACE=$RHDG_NAMESPACE -p CLUSTER_NAME=$RHDG_CLUSTER_NAME | oc apply -f -
+oc process -f templates/rhdg-04-monitoring.yaml \
+    -p CLUSTER_NAMESPACE=$RHDG_NAMESPACE \
+    -p CLUSTER_NAME=$RHDG_CLUSTER_NAME | oc apply -f -
 
 
 ##
@@ -78,14 +86,16 @@ oc process -f templates/rhdg-04-monitoring.yaml -p CLUSTER_NAMESPACE=$RHDG_NAMES
 
 # Deploy the Grafana Operator
 echo -e "\n[5/8]Deploying the Grafana operator"
-oc process -f templates/grafana-01-operator.yaml -p OPERATOR_NAMESPACE=$GRAFANA_NAMESPACE | oc apply -f -
+oc process -f templates/grafana-01-operator.yaml \
+    -p OPERATOR_NAMESPACE=$GRAFANA_NAMESPACE | oc apply -f -
 
 echo -n "Waiting for pods ready..."
 while [[ $(oc get pods -l name=grafana-operator -n $GRAFANA_NAMESPACE -o 'jsonpath={..status.conditions[?(@.type=="Ready")].status}') != "True" ]]; do echo -n "." && sleep 1; done; echo -n -e "  [OK]\n"
 
 # Create a Grafana instance
 echo -e "\n[6/8]Creating a grafana instance"
-oc process -f templates/grafana-02-instance.yaml -p OPERATOR_NAMESPACE=$GRAFANA_NAMESPACE | oc apply -f -
+oc process -f templates/grafana-02-instance.yaml \
+    -p OPERATOR_NAMESPACE=$GRAFANA_NAMESPACE | oc apply -f -
 
 echo -n "Waiting for ServiceAccount ready..."
 while ! oc get sa grafana-serviceaccount -n $GRAFANA_NAMESPACE &> /dev/null; do   echo -n "." && sleep 1; done; echo -n -e " [OK]\n"
@@ -95,12 +105,16 @@ BEARER_TOKEN=$(oc serviceaccounts get-token grafana-serviceaccount -n $GRAFANA_N
 
 # Create a Grafana data source
 echo -e "\n[7/8]Creating the Grafana data source"
-oc process -f templates/grafana-03-datasource.yaml -p BEARER_TOKEN=$BEARER_TOKEN -p OPERATOR_NAMESPACE=$GRAFANA_NAMESPACE | oc apply -f -
+oc process -f templates/grafana-03-datasource.yaml \
+    -p BEARER_TOKEN=$BEARER_TOKEN \
+    -p OPERATOR_NAMESPACE=$GRAFANA_NAMESPACE | oc apply -f -
 
 # Create a Grafana dashboard
 echo -e "\n[8/8]Creating the Grafana dashboard"
 oc create configmap $GRAFANA_DASHBOARD --from-file=dashboard=grafana/$GRAFANA_DASHBOARD.json -n $GRAFANA_NAMESPACE
-oc process -f templates/grafana-04-dashboard.yaml -p DASHBOARD_NAME=$GRAFANA_DASHBOARD -p OPERATOR_NAMESPACE=$GRAFANA_NAMESPACE | oc apply -f -
+oc process -f templates/grafana-04-dashboard.yaml \
+    -p DASHBOARD_NAME=$GRAFANA_DASHBOARD \
+    -p OPERATOR_NAMESPACE=$GRAFANA_NAMESPACE | oc apply -f -
 
 # Print Grafana credentials
 GRAFANA_ADMIN=$(oc get secret grafana-admin-credentials -n $GRAFANA_NAMESPACE -o jsonpath='{.data.GF_SECURITY_ADMIN_USER}' | base64 --decode)
